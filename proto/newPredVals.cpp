ac46ffc23e5e8eb867936a92c7dc3045c130d974
Brenton Kenkel <brenton.kenkel@gmail.com>
2014-01-31 15:37:09 -0500

    Add C++ function to compute polynomial expansions
    This function works row-by-row, so that we never have to store the
    entire N x polynomial-terms(k) matrix in memory at once.  It is not
    meant to be called directly by the user and will probably end up in a
    header file in the final rewritten version of the package.

--- proto/newPredVals.cpp
+++ proto/newPredVals.cpp
@@ -3,3 +3,30 @@
 
 using namespace Rcpp;
 
+// Compute the polynomial expansion of a vector, according to a 'poly_terms'
+// object as returned by polym2()
+NumericVector rawToPoly(NumericVector x, IntegerMatrix poly_terms) {
+    int n_poly = poly_terms.nrow();
+    int n_variables = poly_terms.ncol();
+
+    if (x.size() != n_variables)
+        stop("'x' must be the same length as the number of columns in 'poly_terms'");
+
+    NumericVector ans(n_poly + 1, 1.0); // Initialize each as 1 since we're
+                                        // multiplying within the loop
+    IntegerVector powers;
+
+    // Outside loop: Each row of the polyTerms matrix
+    for (int i = 0; i < n_poly; i++) {
+        // Inside loop: Each element of the row, which is the power of the
+        // corresponding variable of the x vector
+        powers = poly_terms.row(i);
+        for (int j = 0; j < n_variables; j++) {
+            if (powers[j] > 0)
+                ans[i+1] *= pow(x[j], powers[j]);
+        }
+    }
+
+    return ans;
+}
+
