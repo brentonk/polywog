6df6478b226d17018feb51efb989c164e19005e1
Brenton Kenkel <brenton.kenkel@gmail.com>
2014-01-31 11:45:22 -0500

    Remove C++ quantile implementation
    The hand-rolled implementation takes longer than the native R version,
    per microbenchmark(), so I may as well just call the R version from
    within the C++ code

--- proto/newPredVals.cpp
+++ proto/newPredVals.cpp
@@ -1,40 +1,4 @@
 #include <Rcpp.h>
-#include <algorithm>
 
 using namespace Rcpp;
 
-// Reimplementation of the R function quantile(..., type = 7), the default
-// type, as described in ?quantile
-
-// [[Rcpp::export]]
-NumericVector quantile7(NumericVector x, NumericVector probs) {
-    // Check validity of probs
-    if (is_true(any(probs < 0)) || is_true(any(probs > 1)))
-        stop("'probs' must all be between 0 and 1");
-
-    // Set up output vector and intermediate values
-    int n = x.size();
-    int len_probs = probs.size();
-    int j;
-    NumericVector y = clone(x);
-    NumericVector res(len_probs);
-    double lo, hi, p, m, gamma;
-
-    // Compute the quantile corresponding to each element of probs
-    for (int i = 0; i < len_probs; i++) {
-        p = probs[i];
-        m = 1 - p;
-        j = floor(n*p + m);
-        gamma = n*p + m - j;
-
-        // Grab the j'th and (j+1)'th order statistics
-        std::nth_element(y.begin(), y.begin() + j - 1, y.end());
-        lo = y[j-1];
-        std::nth_element(y.begin(), y.begin() + j, y.end());
-        hi = y[j];
-
-        res[i] = (1 - gamma) * lo + gamma * hi;
-    }
-
-    return res;
-}
