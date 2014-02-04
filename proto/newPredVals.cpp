c86f79c8c9f3faaa6efadb06888e67e0a21f44e0
Brenton Kenkel <brenton@veblen>
2014-02-04 16:09:12 -0500

    Implement the beef of predict/predVals in C++

--- proto/newPredVals.cpp
+++ proto/newPredVals.cpp
@@ -58,3 +58,96 @@ NumericVector columnFromSparse(S4 X, int j) {
     return res;
 }
 
+// [[Rcpp::export]]
+List newPredictPolywogC(NumericMatrix X,
+                        IntegerMatrix poly_terms,
+                        List coef,
+                        bool avg,
+                        bool interval,
+                        double level)
+{
+    // Storage for final output
+    //
+    // avg -> just a single average and its confidence interval
+    // interval -> prediction and confidence interval for each observation
+    // neither -> just a prediction for each observation
+    int n_obs = X.nrow();
+    NumericVector predicted(avg ? 1 : n_obs, 0.0);
+    NumericVector low(avg ? 1 :
+                      interval ? n_obs :
+                      0);
+    NumericVector high(avg ? 1 :
+                       interval ? n_obs :
+                       0);
+
+    // Extract the main coefficients and the bootstrap matrix
+    NumericVector coef_main = coef["main"];
+    int n_boot = 0;
+    S4 boot_matrix;
+    if (avg || interval) {
+        boot_matrix = as<S4>(coef["boot"]);
+        IntegerVector dim_boot = boot_matrix.slot("Dim");
+        n_boot = dim_boot[1];  // Bootstrap iterations = number of columns
+    }
+    NumericVector fit_boot(n_boot, 0.0);
+
+    // Set up for the confidence interval calculations
+    Environment stats("package:stats");
+    Function quantile = stats["quantile"];
+    NumericVector probs(avg || interval ? 2 : 0);
+    if (avg || interval) {
+        probs[0] = 0.5 - (level/2);
+        probs[1] = 1 - probs[0];
+    }
+
+
+    // Loop over each row (observation) in X
+    for (int i = 0; i < n_obs; i++) {
+        // Calculate predicted values from the main model fit
+        NumericVector x_row = X.row(i);
+        NumericVector x_row_poly = rawToPoly(x_row, poly_terms);
+        if (avg) {
+            predicted[0] += sum(x_row_poly * coef_main) / n_obs;
+        } else {
+            predicted[i] = sum(x_row_poly * coef_main);
+        }
+
+        // Loop through the bootstrap coefficients
+        if (avg || interval) {
+            for (int j = 0; j < n_boot; j++) {
+                NumericVector coef_boot = columnFromSparse(boot_matrix, j);
+
+                if (avg) {
+                    fit_boot[j] += sum(x_row_poly * coef_boot) / n_boot;
+                } else {
+                    fit_boot[j] = sum(x_row_poly * coef_boot);
+                }
+            }
+
+            // The confidence interval is per-observation in the 'interval'
+            // case, so compute it inside the loop
+            if (!avg) {
+                NumericVector conf_int = quantile(fit_boot, probs);
+                low[i] = conf_int[0];
+                high[i] = conf_int[1];
+            }
+        }
+    }
+
+    // In the 'average' case, the confidence interval is for the mean, rather
+    // than for each individual prediction, so compute it outside the loop
+    // over bootstrap coefficients
+    if (avg) {
+        NumericVector conf_int = quantile(fit_boot, probs);
+        low[0] = conf_int[0];
+        high[0] = conf_int[1];
+    }
+
+    if (avg || interval) {
+        return List::create(_["pred"] = predicted,
+                            _["lwr"] = low,
+                            _["upr"] = high);
+    } else {
+        return List::create(_["pred"] = predicted);
+    }
+}
