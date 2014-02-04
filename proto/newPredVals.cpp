9c90fcca5ad21924b151702cbc1d383fa80914e4
Brenton Kenkel <brenton@veblen>
2014-02-04 16:04:10 -0500

    Add C++ function to polynomial-expand a matrix row

--- proto/newPredVals.cpp
+++ proto/newPredVals.cpp
@@ -29,3 +29,32 @@ NumericVector rawToPoly(NumericVector x, IntegerMatrix poly_terms) {
     return ans;
 }
 
+// Extract a specified column (by zero-based index) from a sparse matrix
+NumericVector columnFromSparse(S4 X, int j) {
+    // Set up the output as an appropriately-sized vector of 0s
+    IntegerVector dim = X.slot("Dim");
+    int nrow = dim[0];
+    NumericVector res(nrow, 0.0);
+
+    // The values of a "dgCMatrix" object X are stored as follows:
+    //   o X@p contains the cumulative number of non-zero values by column
+    //   o The indices of the non-zero values of the j'th column are contained
+    //     in X@i[X@p[j]:(X@p[j+1]-1)]
+    //   o The corresponding values are stored in X@x[X@p[j]:(X@p[j+1]-1)]
+    IntegerVector p = X.slot("p");
+    int ind_start = p[j];
+    int ind_end = p[j+1];
+
+    // Loop through the indices corresponding to the j'th column and fill in
+    // the result vector appropriately
+    int ind;
+    IntegerVector indices = X.slot("i");
+    NumericVector values = X.slot("x");
+    for (int i = ind_start; i < ind_end; i++) {
+        ind = indices[i];
+        res[ind] = values[i];
+    }
+
+    return res;
+}
+
