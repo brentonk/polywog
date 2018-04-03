486496b98633b636401fd5a754eef8191f6206b6
Brenton Kenkel <brenton.kenkel@gmail.com>
2018-04-03 11:57:05 -0400

    Replace deprecated Matrix::cBind

--- R/bootPolywog.r
+++ R/bootPolywog.r
@@ -55,7 +55,7 @@
 ##' @example inst/examples/bootPolywog.r
 ##' @import foreach
 ##' @import iterators
-##' @importFrom Matrix Matrix cBind
+##' @importFrom Matrix Matrix
 bootPolywog <- function(model,
                         nboot = 100,
                         .parallel = FALSE,
@@ -186,7 +186,7 @@ bootPolywog <- function(model,
 
     ## Construct the bootstrap matrix, with each column being a set of
     ## coefficients (*not* each row as in v0.3.0 and earlier)
-    ans <- do.call(cBind, ans)
+    ans <- do.call(cbind, ans)
     rownames(ans) <- names(coef(model))
 
     ## If .matrixOnly (typically only used within 'polywog'), return only the
